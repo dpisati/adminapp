1b95916295f3857788b37ab06e4b5bf5c89d6d5e
dpisati <43629737+dpisati@users.noreply.github.com>
2020-05-15 16:44:21 +1200

    css.js.no.asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,10 +14,12 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
         <!-- Styles -->
-        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+        {{-- <link href="{{ asset('css/main.css') }}" rel="stylesheet"> --}}
+        <link href="/css/main.css" rel="stylesheet">
 
          <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+        <script src="/js/app.js" defer></script>
 
     </head>
     <body>
