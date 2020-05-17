9e898445b004055e4ca2a1e5b225bedb1bbe0bf5
dpisati <dpisati@gmail.com>
2020-05-18 09:52:00 +1200

    popper

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,9 @@
 const mix = require("laravel-mix");
 
+mix.sourceMaps()
+    .js("node_modules/popper.js/dist/popper.js", "public/js")
+    .sourceMaps();
+
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .sass("resources/sass/main.scss", "public/css");
