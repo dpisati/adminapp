2a7841cd51b07f843eb616d6a9865bd4f5209d92
dpisati <dpisati@gmail.com>
2020-05-20 15:55:47 +1200

    added_pictures2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,7 +120,7 @@
     <div class="customers">
         <div class="customers-title">
             <hr>
-            <h3>Premium Customers</h3>
+            <h3>Premium Custmers</h3>
             <hr>
         </div>
         <div class="customer-logos">
